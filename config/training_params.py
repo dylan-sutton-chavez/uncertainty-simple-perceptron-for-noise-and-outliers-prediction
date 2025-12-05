25ee5e5aa099616ac2c9cc86203aaa22c0f54e5b
Dylan Sutton Chavez <c.sutton.dylan@gmail.com>
2025-12-05 08:49:15 -0600

    Update training_params.py

--- config/training_params.py
+++ config/training_params.py
@@ -2,4 +2,3 @@ EPOCHS: int = 170 # Maximum number of full training cycles to run.
 PATIENCE: int = 13 # Epochs to wait before triggering early stopping.
 
 LEARNING_RATE: float = 0.0001 # Controls how much the weights adjust during the training.
-
